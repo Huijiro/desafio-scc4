0f5f5a44b7aea955317c8abea65d8a46cb994f34
Gabriel Rodrigues Campos <huijirohankei@gmail.com>
2023-04-26 09:02:00 -0300

    git: Adicionado exclusão do banco de dados.

--- backend/src/main/java/com/scc4/purejs/controller/ClienteController.java
+++ backend/src/main/java/com/scc4/purejs/controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.scc4.purejs.mapper.ClienteRowMapper;
 import com.scc4.purejs.model.Cliente;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/cliente")
 public class ClienteController {
     @Autowired
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,22 @@
 import './style.css'
 
+
+// Constantes
+
+const SERVER_ADDRESS = 'http://localhost:8080';
+
 const form = document.querySelector('#input') as HTMLFormElement;
 const tableBody = document.querySelector('#data-body') as HTMLTableSectionElement;
+
+// Interfaces
+
+interface Cliente {
+    id?: number;
+    nome: string;
+    email: string;
+    telefone: string;
+    profissao: string;
+}
 
 interface FormElements extends HTMLFormControlsCollection {
     nome: HTMLInputElement;
@@ -10,7 +25,25 @@ interface FormElements extends HTMLFormControlsCollection {
     profissao: HTMLInputElement;
 }
 
-form.addEventListener('submit', (e: SubmitEvent) => {
+// Funções que afetam a tabela.
+
+async function loadTable() {
+  console.log('Carregando tabela...');
+  
+    while(tableBody.firstChild) {
+        tableBody.firstChild.remove();
+    }
+    const clientes = await getClientes();
+    clientes.forEach(cliente => {
+        const tableRow = addTableRow(cliente);
+        tableBody.appendChild(tableRow);
+    })
+
+}
+
+loadTable();
+
+form.addEventListener('submit', async (e: SubmitEvent) => {
   e.preventDefault();
   const form = e.target as HTMLFormElement & { elements: FormElements };
   const nome = form.elements['nome'].value;
@@ -18,35 +51,86 @@ form.addEventListener('submit', (e: SubmitEvent) => {
   const telefone = form.elements['telefone'].value;
   const profissao = form.elements['profissao'].value;
 
-  const data = {
+  const cliente: Cliente = {
       nome,
       email,
       telefone,
       profissao
   }
 
-  const tableRow = document.createElement('tr');
-  const tableDataId = document.createElement('td');
-  const tableDataNome = document.createElement('td');
-  const tableDataEmail = document.createElement('td');
-  const tableDataTelefone = document.createElement('td');
-  const tableDataProfissao = document.createElement('td');
-  const tableDataAcoes = document.createElement('td');
+  await postCliente(cliente);
 
-
-  tableDataId.textContent = '1';
-  tableDataNome.textContent = data.nome;
-  tableDataEmail.textContent = data.email;
-  tableDataTelefone.textContent = data.telefone;
-  tableDataProfissao.textContent = data.profissao;
-  tableDataAcoes.innerHTML = "Excluir";
-
-  tableRow.appendChild(tableDataId);
-  tableRow.appendChild(tableDataNome);
-  tableRow.appendChild(tableDataEmail);
-  tableRow.appendChild(tableDataTelefone);
-  tableRow.appendChild(tableDataProfissao);
-  tableRow.appendChild(tableDataAcoes);
-
-  tableBody.appendChild(tableRow);
+  await loadTable();
 })
+
+function addTableRow(cliente: Cliente): HTMLTableRowElement {
+    const tableRow = document.createElement('tr');
+    const tableDataId = document.createElement('td');
+    const tableDataNome = document.createElement('td');
+    const tableDataEmail = document.createElement('td');
+    const tableDataTelefone = document.createElement('td');
+    const tableDataProfissao = document.createElement('td');
+    const tableDataAcoes = document.createElement('td');
+
+    tableRow.appendChild(tableDataId);
+    tableRow.appendChild(tableDataNome);
+    tableRow.appendChild(tableDataEmail);
+    tableRow.appendChild(tableDataTelefone);
+    tableRow.appendChild(tableDataProfissao);
+    tableRow.appendChild(tableDataAcoes);
+
+    tableDataId.textContent = cliente.id!.toString();
+    tableDataNome.textContent = cliente.nome;
+    tableDataEmail.textContent = cliente.email;
+    tableDataTelefone.textContent = cliente.telefone;
+    tableDataProfissao.textContent = cliente.profissao;
+
+    const button = document.createElement('button');
+
+    button.textContent = 'Excluir';
+    button.className = 'button-excluir';
+
+    button.addEventListener('click', async () => {
+        await deleteCliente(cliente.id!);
+    })
+
+    tableDataAcoes.appendChild(button);
+    
+    return tableRow;
+}
+
+// Funções que usam o servidor.
+
+async function getClientes(): Promise<Cliente[]> {
+    const response = await fetch(`${SERVER_ADDRESS}/cliente`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const data = await response.json();
+    return data;
+}
+
+async function postCliente(cliente: Cliente): Promise<Cliente> {
+    const response = await fetch(`${SERVER_ADDRESS}/cliente`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(cliente)
+    });
+    const data = await response.json();
+    return data;
+}
+
+async function deleteCliente(id: number): Promise<Cliente> {
+    const response = await fetch(`${SERVER_ADDRESS}/cliente/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const data = await response.json();
+    return data;
+}
